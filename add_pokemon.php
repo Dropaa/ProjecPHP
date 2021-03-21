441b8d74e13e5a1f33684d93f5fd306f30a7c057
Vitaalx <liamdu92@gmail.com>
2021-03-21 21:57:18 +0100

    create add_pokemon.php and connexion.php

--- add_pokemon.php
+++ add_pokemon.php
@@ -1,1 +1,74 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+</head>
+
+<style>
+
+    body{
+        font-family: 'Lato', sans-serif;
+    }
+    .add_pokemon{
+        margin-left: 461px;
+        margin-right: 461px;
+        margin-top: 200px ;
+    }
+
+    h1{
+        padding: 10px;
+        text-align: center;
+    }
+
+    .nom,.pv, .attaque, .défense, .vitesse, .ajouter {
+        border-radius: 5px;
+        border: 1px solid;
+        border-color: grey;
+        font-size: 16px;
+    }
+
+    .file{
+        padding-left: 5px;
+    }
+
+    ::placeholder{
+        color: #626978;
+    }
+    .nom, .pv, .attaque, .défense, .vitesse {
+        display: inline-block;
+        width: 100%;
+        padding: 12px;
+        margin-bottom: 10px;
+    }
+    .ajouter{
+        border: hidden;
+        color: white;
+        width: 103%;
+        padding: 12px;
+        background-color: #4DAE50;
+        font-size: 16px;
+    }
+
+    .ajouter:hover{
+        opacity: .9;
+    }
+
+</style>
+
+<body>
+<div class="add_pokemon">
+    <h1><strong>AJOUTER UN POKEMON</strong></h1>
+    <form action="profile.php" role="form" method="post">
+        <input type="text" name="nom" placeholder="Nom" class="nom" required>
+        <input type="text" name="pv" placeholder="PV" class="pv" required>
+        <input type="text" name="attaque" placeholder="Attaque" class="attaque" required>
+        <input type="text" name="défense" placeholder="Défense" class="défense" required>
+        <input type="text" name="vitesse" placeholder="Vitesse" class="vitesse" required>
+        <p><strong>Image :</strong><input type="file" accept="image/png" class="file" required></p>
+        <input type="submit" value="Ajouter" class="ajouter">
+    </form>
+</div>
+</body>
+</html>
--- connexion.php
+++ connexion.php
@@ -1,1 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <h1><strong>CONNEXION</strong></h1>
+
+<div class="connexion">
+    <h1>Je possède un compte</h1>
+    <form action="verif/verif_connexion.php" role="form" method="post">
+
+        <input type="email" name="email" placeholder="E-mail" class="emailConnexion" required>
+        <input type="password" name="password" placeholder="Mot de passe" class="passwordConnexion" required>
+
+        <input type="submit" value="Connexion" class="boutonConnexion">
+
+    </form>
+</div>
+
+<div class="inscription">
+    <h1>Je crée un compte</h1>
+    <form action="verif/verif_inscription.php" role="form" method="post">
+
+        <input type="text" name="nickname" placeholder="Pseudo" class="nicknameInscription" required>
+        <input type="email" name="email" placeholder="E-mail" class="emailInscription" required>
+        <input type="password" name="password" placeholder="Mot de passe" class="passwordInscription" required>
+        <p><strong>Image de profil :</strong> <input type="file" accept="image/png" class="fileInscription" required></p>
+        <input type="submit" value="Inscription" class="boutonInscription">
+
+    </form>
+</div>
+
+</body>
+
+</html>
